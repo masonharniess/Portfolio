ca4f3ede770e450f099a5e582a79b53c75f45157
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2024-12-06 16:37:35 +0000

    feat: remove Next.js boilerplate
    This removes the boilerplate messaging from the main page of the app.

---
+++ portfolio-client/src/app/(pages)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+
+        <p>Website by Mason Harniess.</p>
+
+      </main>
+    </div>
+  );
+}
