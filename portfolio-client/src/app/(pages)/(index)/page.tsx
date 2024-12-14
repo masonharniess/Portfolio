ebe312bbf2e44979f3bd40d1116ee20336367c5b
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2024-12-14 13:26:02 +0000

    feat: remove miscellaneous UI items from home page

--- portfolio-client/src/app/(pages)/(index)/page.tsx
+++ portfolio-client/src/app/(pages)/(index)/page.tsx
@@ -1,35 +1,31 @@
 import styles from "../../page.module.css";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <p>Website by Mason Harniess.</p>
-        <Image src="/taariq.jpeg" width="300" height="300" alt=""/>
-        <MyButton/>
-        <Profile/>
       </main>
     </div>
   );
 }
-
-function MyButton() {
-  return (
-    <button className="avatar">Im a button</button>
-  )
-}
-
-const user = {
-  name: "Mason Harniess"
-}
-
-function Profile() {
-  return (
-    <>
-      <h1>
-        {user.name}
-      </h1>
-    </>
-  )
-}
+//
+// function MyButton() {
+//   return (
+//     <button className="avatar">Im a button</button>
+//   )
+// }
+//
+// const user = {
+//   name: "Mason Harniess"
+// }
+//
+// function Profile() {
+//   return (
+//     <>
+//       <h1>
+//         {user.name}
+//       </h1>
+//     </>
+//   )
+// }
