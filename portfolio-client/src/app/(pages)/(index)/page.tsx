8b7052dd804f8d425f38e1b5d6274d5587a7ae87
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2024-12-14 16:04:55 +0000

    feat: add 'work in progress' message on the home page

--- portfolio-client/src/app/(pages)/(index)/page.tsx
+++ portfolio-client/src/app/(pages)/(index)/page.tsx
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
-import {inknut_antiqua_bold} from "@/app/fonts/fonts";
+import {inknut_antiqua_light, inknut_antiqua_bold} from "@/app/fonts/fonts";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <p className={inknut_antiqua_bold.className}>Website by Mason Harniess.</p>
+        <p className={inknut_antiqua_light.className}>Work in progress.</p>
       </main>
     </div>
   );
--- portfolio-client/src/app/fonts/fonts.ts
+++ portfolio-client/src/app/fonts/fonts.ts
@@ -5,6 +5,11 @@ export const inknut_antiqua_bold = Inknut_Antiqua({
   weight: ["700"]
 })
 
+export const inknut_antiqua_light = Inknut_Antiqua({
+  subsets: ["latin"],
+  weight: ["300"]
+})
+
 export const montserrat_bold = Montserrat({
   subsets: ["latin"],
   weight: ["700"]
