8a8d17dc1a89def35d4cbe6f6add6301fb60aa7b
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-04-03 16:28:04 +0100

    fix: safari viewport
    This commit removes the white space above and below the main view on safari mobile when using a notched iPhone.

--- portfolio-client/src/app/(pages)/(index)/page.tsx
+++ portfolio-client/src/app/(pages)/(index)/page.tsx
@@ -6,7 +6,6 @@ import IntroCard from "@/app/components/intro/intro-card";
 import LastfmCard from "@/app/components/lastfm/lastfm-card";
 import Wallpaper from "@/app/components/wallpaper/wallpaper";
 
-
 export default function Home() {
   return (
     <div className={styles.page}>
--- portfolio-client/src/app/layout.tsx
+++ portfolio-client/src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
 
+import type { Viewport } from 'next'
+
+export const viewport: Viewport = {
+  themeColor: 'black', //54595B
+  width: '100%',
+  viewportFit: `cover`
+}
+
 export const metadata: Metadata = {
   title: "Mason Harniess",
   description: "A portfolio of work.",
