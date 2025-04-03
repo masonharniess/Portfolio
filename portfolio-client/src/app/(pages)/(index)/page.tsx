0c87cb89f7e15911a6d8a912d079bcf44f4ba100
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-04-03 15:20:35 +0100

    style: add floating orbs aesthetic
    This adds blurred floating orbs to the background to improve the style.

--- portfolio-client/src/app/(pages)/(index)/page.tsx
+++ portfolio-client/src/app/(pages)/(index)/page.tsx
@@ -4,13 +4,14 @@ import IntroCard from "@/app/components/intro/intro-card";
 // import LastfmCard from "@/app/components/lastfm/lastfm-card";
 // import TestCard from "@/app/components/testing/test-card";
 import LastfmCard from "@/app/components/lastfm/lastfm-card";
+import Wallpaper from "@/app/components/wallpaper/wallpaper";
 
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      {/*<img className={styles.background}/>*/}
-      <main className={styles.main}>
+      <Wallpaper />
+      <main className={styles.content}>
         <IntroCard/>
         <LastfmCard/>
       </main>
--- portfolio-client/src/app/components/wallpaper/wallpaper.tsx
+++ portfolio-client/src/app/components/wallpaper/wallpaper.tsx
@@ -3,9 +3,9 @@ import styles from "./wallpaper.module.css"
 export default function Wallpaper() {
   return (
     <div className={styles.gradient_background}>
-      <div className={styles.gradient_sphere_1}></div>
-      <div className={styles.gradient_sphere_2}></div>
-      <div className={styles.gradient_sphere_3}></div>
+      <div className={`${styles.gradient_sphere} ${styles.sphere_1}`}></div>
+      <div className={`${styles.gradient_sphere} ${styles.sphere_2}`}></div>
+      <div className={`${styles.gradient_sphere} ${styles.sphere_3}`}></div>
       <div className={styles.glow}></div>
       <div className={styles.grid_overlay}></div>
       <div className={styles.noise_overlay}></div>
