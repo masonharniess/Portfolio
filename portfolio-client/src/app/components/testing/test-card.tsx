c3d9bdd2c8e10c30eba76d7a207c6ed4b2078484
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-04-04 22:47:47 +0100

    fix: remove handleClick() that was causing deployment issue

--- portfolio-client/src/app/components/testing/test-card.tsx
+++ portfolio-client/src/app/components/testing/test-card.tsx
@@ -3,9 +3,7 @@ import {useState} from "react";
 export default function MyApp() {
   const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+
 
   return (
     <div>
