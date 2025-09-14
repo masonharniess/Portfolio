10b206c66fe447c4aa097f5eedbf97f429b0f53c
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-09-14 19:40:41 +0100

    remove: delete backend
    This commit removes the backend, as it had no purpose anymore.

--- portfolio-server/Models/ToDoItem.cs
+++
@@ -1,8 +0,0 @@
-namespace portfolio_server.Models;
-
-public class ToDoItem
-{
-    public Guid Id { get; set; }
-    public string? Title { get; set; }
-    public bool IsComplete { get; set; }
-}
--- portfolio-server/Services/Lastfm/ILastfmService.cs
+++
@@ -1,7 +0,0 @@
-using portfolio_server.Models.LastFM;
-namespace portfolio_server.Services.LastFM;
-
-public interface ILastfmService
-{
-    Task<List<TrackDto>> GetRecentTracksAsync(string user, int limit);
-}
